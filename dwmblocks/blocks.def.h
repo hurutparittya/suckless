5af9ed1d44655b876adf34e08dbab40af6496819
hurutparittya <none>
2021-09-08 09:25:07 +0200

    added battery indicator

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/		/*Command*/				/*Update Interval*/	/*Update Signal*/
-	{	"", "echo 盛 $(($(cat /sys/class/backlight/intel_backlight/brightness) / 44))%", 0, 2},
-	{	"",	"amixer get Master | awk -F'[][]' 'END{ ORS=\"\"; if ($6 == \"on\") {print \" \"$2} else {print \"婢\"}}'",	0,	1},
-	{	"",	"date +' %H:%M   %Y %m %d   '",	10,	0},
+	{	" ",	"echo $(cat /sys/class/power_supply/BAT0/capacity)% status: $(cat /sys/class/power_supply/BAT0/status)",	3,	0},
+	{	"盛 ",	"echo $(($(cat /sys/class/backlight/intel_backlight/brightness) / 44))%", 0, 2},
+	{	"",		"amixer get Master | awk -F'[][]' 'END{ ORS=\"\"; if ($6 == \"on\") {print \" \"$2} else {print \"婢\"}}'",	0,	1},
+	{	"",		"date +' %H:%M   %Y %m %d '",	10,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
